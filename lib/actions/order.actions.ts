f34f5661119d94dbc40c76cc76833839776a7b8c
John Stoops <john.stoops@gmail.com>
2025-02-24 11:04:25 -1000

    Added webhook for Stripe

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -203,7 +203,7 @@ export async function approvePayPalOrder(
 }
 
 // Update order to paid
-async function updateOrderToPaid({
+export async function updateOrderToPaid({
   orderId,
   paymentResult,
 }: {
